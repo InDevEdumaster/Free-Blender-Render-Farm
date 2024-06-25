d16ab5ff97790847e2a7229efa0de62c8e43d242
InDevEdumaster <shenal.alevel@gmail.com>
2024-06-26 03:42:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://imagebinaryconverter.000webhostapp.com/login.php', true); // Replace with your 000webhost site URL
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            alert(xhr.responseText);
+        } else {
+            alert('Request failed. Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send('username=' + username + '&password=' + password);
+});
+
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://imagebinaryconverter.000webhostapp.com/register.php', true); // Replace with your 000webhost site URL
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            alert(xhr.responseText);
+        } else {
+            alert('Request failed. Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send('username=' + username + '&password=' + password);
+});
